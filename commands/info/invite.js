d4687e4587462abf96e5dfcf7a38d82a0a39c325
Jacob1Tm <j.pawlak.2007@gmail.com>
2022-07-22 18:31:15 +0200

    :2Head:

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -3,10 +3,10 @@ module.exports = {
     name: 'invite',
     description: 'Wysyła link z zaproszeniem bota',
     category: 'fun',
-    execute(message) {
+    execute(message, args, client) {
         const embed = new Discord.MessageEmbed()
         embed.setFooter(`Komenda wykonana przez ${message.author.tag}`, `${message.author.displayAvatarURL()}`)
-        embed.setDescription(`Dodaj bota na swój serwer\n[Link](https://discord.com/oauth2/authorize?client_id=${client.clientId}&permissions=269872375&scope=bot+applications.commands)`)
+        embed.setDescription(`Dodaj bota na swój serwer\n[Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=269872375&scope=bot+applications.commands)`)
         message.channel.send({embeds: [embed]})
     },
 };
