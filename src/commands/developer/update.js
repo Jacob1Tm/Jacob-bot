038d9c601d82c37222b78785c155c700e4307b0a
Jakup <jacobtm@jacobtm.tk>
2022-11-28 21:11:41 +0100

    wywalam to bo mnie wkurza

--- src/commands/developer/update.js
+++ src/commands/developer/update.js
@@ -5,13 +5,6 @@ module.exports = {
     category: 'developer',
     ownerOnly: true,
     async execute(message) {
-        //aby uruchomić komendę upewnij się że git nie będzie wymagał logowania oraz czy po wyłączeniu procesu manager
-        //taki jak np pm2 uruchomi proces ponownie
-        //jeżeli wszystko jest zrobione zmień w off na false lub usuń wraz z ifem i else
-        const off = true;
-        if (off === true) {
-            message.channel.send("Hej ta komenda jest wyłączona w samym pliku po dalsze informacje zajrzyj do \`update.js\`");
-            } else {
             message.channel.send("Pobrano aktualizacje bot się zresetuje automatycznie.")
             const {exec} = require("child_process");
             exec(`git pull`, (error, stdout, stderr) => {
@@ -19,5 +12,4 @@ module.exports = {
                 process.exit()
             })
         }
-    }
 };
