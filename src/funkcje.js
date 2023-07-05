f3a87e26497de2381efcceb435abc46a934a39c1
xKubsoneQ <63014799+xKubsoneQ@users.noreply.github.com>
2023-07-05 20:31:14 +0200

    przeciez tego crowdina nie bedzie

--- src/funkcje.js
+++ src/funkcje.js
@@ -12,8 +12,6 @@ module.exports = {
             `Używany przez ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}  użytkowników`,
             `Prefiks: ${config.prefix}`,
             `Wersja: ${global.v}`,
-            `Soon™️ in English (maybe)`,
-            `Help translating on crowdin (soon)`
         ]
         client.user.setActivity(statuses[Math.floor(Math.random() * statuses.length)], { type: "WATCHING" });
     },
@@ -46,7 +44,6 @@ module.exports = {
     },
     async djCheck(message) {
         let result = true;
-        if (global.databaseonline === false) return result = false;
         if (global.databaseonline === true) {
             const djModel = require("./modele/guildDjRoleSchema");
             let server = await djModel.findOne({guildID: `${message.guild.id}`})
@@ -54,6 +51,6 @@ module.exports = {
             let djRole = server.djRoleID
             if (message.member.roles.cache.has(djRole)) return result = true;
             else return result = false;
-        }
+        } else return result = false;
     },
 }
